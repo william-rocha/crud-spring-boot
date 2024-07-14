4910a3b08fedbceb148910b865452c338d5a6bbd
WILLIAM_ROCHA <williamrochajr@gmail.com>
2024-07-13 22:24:58 -0300

    initial commit; hello

---
+++ src/main/java/com/example/crud_spring/CrudSpringApplication.java
@@ -0,0 +1,16 @@
+package com.example.crud_spring;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/crud_spring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
